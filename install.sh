17669fda94e560df6ce8e40063ac10f5a95da18f
izumi0x01 <izumisoh.develop@gmail.com>
2024-07-29 01:58:56 +0900

    [change]install.shのリファクト

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ oh_my_posh(){
   cd ~/.local/share/fonts
   curl -fLo "Hack Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
   fc-cache -fv
+  cd ~
 
   # .bashrcにOh My Poshの設定を追加
   THEME="paradox" 
