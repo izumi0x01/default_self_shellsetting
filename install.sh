3f61aebc9532c154be35ea6d8d05876fc6562550
izumi0x01 <izumisoh.develop@gmail.com>
2024-08-07 04:30:20 +0900

    [change]install.shにおいてoh-my-poshのインストールファイルを編集

--- install.sh
+++ install.sh
@@ -5,32 +5,60 @@ show_help(){
   echo 
   echo "these are common install command"
   echo "  --help"
+  echo "  --nerd"
   echo "  --oh-my-posh"
   echo "  --tmux"
 }
 
-initial_install(){
-  sudo apt install zip 
-  sudo apt install unzip 
+# 引数が指定されているかチェック
+if [ $# -eq 0 ]; then
+    show_help
+    return 0
+fi
+
+validate_dependency() {
+    if ! command -v $1 >/dev/null; then
+        error "$1 is required to install . Please install $1 and try again.\n"
+    fi
+}
+
+validate_dependencies() {
+    validate_dependency wget
+    validate_dependency curl
+    validate_dependency zip
+    validate_dependency unzip
+    validate_dependency realpath
+    validate_dependency dirname
+}
+
+
+install_Nerd_fonts(){
+  mkdir -p ~/.local/share/fonts
+  cd ~/.local/share/fonts
+  curl -fLo "Hack Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
+  fc-cache -fv
+  cd ~
 }
 
 oh_my_posh(){
-  sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-  sudo chmod +x /usr/local/bin/oh-my-posh
+  # oh-my-poshのダウンロード
+  mkdir -p ~/.local/bin
+  curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+  chmod u+x ~/.local/bin/oh-my-posh
+
+  # 環境変数の追加
+  echo 'if [[ "$PATH" != *"$HOME/.local/bin"* ]]; then
+      export PATH="$HOME/.local/bin:$PATH"
+  fi' >> ~/.bash_profile
 
   # テーマのダウンロード
   echo "Downloading themes..."
   mkdir -p ~/.poshthemes
   wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
   unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
-  chmod u+rw ~/.poshthemes/*.omp.*
+  chmod u+rw ~/.poshthemes/*.json
   rm ~/.poshthemes/themes.zip
 
-  mkdir -p ~/.local/share/fonts
-  cd ~/.local/share/fonts
-  curl -fLo "Hack Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
-  fc-cache -fv
-  cd ~
 
   # .bashrcにOh My Poshの設定を追加
   THEME="paradox" 
@@ -46,32 +74,22 @@ oh_my_posh(){
   source ~/.bashrc
 }
 
-oh_my_posh_change_theme(){
-  return 0
-}
 
-# 引数が指定されているかチェック
-if [ $# -eq 0 ]; then
-    show_help
-    return 0
-fi
+validate_dependencies
 
 # 引数解析
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
-        -h|--help)
+        --help)
             show_help
             ;;
-        -o|--oh-my-posh)
+        --nerd)
+            install_Nerd_fonts
+            ;;
+        --oh-my-posh)
             oh_my_posh
             ;;
-        -oc|--oh-my-posh-change-theme)
-            oh_my_posh_change_theme
-            ;;
-        -t|--tmux)
-            ;;
-        -i|--initial-install)
-            initial_install
+        --tmux)
             ;;
         *)
             echo "Unknown option: $1"
